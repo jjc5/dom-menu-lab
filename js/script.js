c503a802d05d98c0146403eb2e495127059c7b1a
Jason C <jayingmantis.555@gmail.com>
2021-05-08 13:40:45 -0500

    Task 2.3 complete

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
+//Task 1.0
 const mainEl = document.querySelector('main');
-
+//Task 1.1
 mainEl.style.backgroundColor = 'var(--main-bg)';
-
+//Task 1.2
 mainEl.innerHTML = '<h1>SEI Rocks!</h1>';
-
+//Task 1.3
 mainEl.classList.add('flex-ctr');
+//Task 2.0
+const topMenuEl = document.getElementById("top-menu");
 
-console.log(mainEl);
+//Task 2.1
+topMenuEl.style.height = '100%';
+console.log(topMenuEl);
+//task 2.2
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+
+//task2.3
+topMenuEl.classList.add('flex-around');
